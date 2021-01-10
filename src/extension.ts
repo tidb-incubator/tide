84cd9518985c2128da35549119c888c529d1c85a
Li Yilong <liyilongko@gmail.com>
2021-01-10 10:55:08 +0800

    experiments

--- src/extension.ts
+++ src/extension.ts
@@ -4,7 +4,7 @@ import * as vscode from 'vscode';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+export async function activate(context: vscode.ExtensionContext) {
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
@@ -13,11 +13,11 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('ticode.helloWorld', () => {
+	let disposable = vscode.commands.registerCommand('ticode.playground', () => {
 		// The code you place here will be executed every time your command is executed
 
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from TiCode!');
+		vscode.window.showInformationMessage('Hello TiCode!');
 	});
 
 	context.subscriptions.push(disposable);
